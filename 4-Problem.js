24b14cf93b8611b01dffd7808965ce2ed600cd17
Anirudha Bele <beleanirudha8@gmail.com>
2025-03-03 11:44:24 +0530

    feat(4-Problem.js): add solution for 4-Problem

---
+++ 4-Problem.js
@@ -0,0 +1,15 @@
+// 4-Problem statement - Remove the last element of an array and log the updated array.
+
+// 3-Problem statement - Add two new elements to the start of an array using `.unshift()`.
+
+const numbersArray = [1, 2, 3, 4, 5];
+
+console.log("Original number array is: ");
+
+console.table(numbersArray);
+
+numbersArray.pop();
+
+console.log("Updated array, after removing last element is: ");
+
+console.table(numbersArray);
