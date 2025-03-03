56cc359b5dcf40a6731e30b0755dd7230496dc2a
Anirudha Bele <beleanirudha8@gmail.com>
2025-03-03 11:29:23 +0530

    feat(1-Problem.js): add solution for 1-Problem

---
+++ 1-Problem.js
@@ -0,0 +1,7 @@
+// 1-Problem statement - Create an array of your top 5 favorite movies and log it.
+
+const favoriteMovies = ["movie1", "movie2", "movie3", "movie4", "movie5"];
+
+// console.log(`My top 5 favorite movies are: ${favoriteMovies}`);
+
+console.log("My top 5 favorite movies are: ", favoriteMovies);
