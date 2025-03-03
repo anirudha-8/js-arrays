a0c0e7425a658adfaa11c2e86a7216242f33b660
Anirudha Bele <beleanirudha8@gmail.com>
2025-03-03 12:06:15 +0530

    feat(8-Problem.js): add solution for 8-Problem

---
+++ 8-Problem.js
@@ -0,0 +1,20 @@
+// 8-Problem statement - Combine two arrays `[1, 2]` and `[3, 4]` using `.concat()`.
+
+const firstNumberArray = [1, 2];
+
+const secondNumberArray = [3, 4];
+
+const combinedArray = firstNumberArray.concat(secondNumberArray);
+
+// console.log(
+// 	"Combined array of ",
+// 	firstNumberArray,
+// 	" and ",
+// 	secondNumberArray,
+// 	" is: ",
+// 	combinedArray
+// );
+
+console.log(
+	`Combined array of [${firstNumberArray}] and [${secondNumberArray}] is - [${combinedArray}]`
+);
